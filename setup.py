24974dcdd4bf95b9e891f7c8550b8066df504042
Joselito Beriguete Nuñez <misiones03@hotmail.com>
2025-07-08 21:12:39 -0400

    Rename package to filevent and restructure tests

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="fileevent",
+    name="filevent",
     version="0.1.0",
     description="Librería para emisión y recepción de eventos usando archivos organizados por VM y usuario.",
     author="Joselito Beriguete",
-    author_email="tunombre@ejemplo.com",
+    author_email="joselito0125@gmail.com",
     packages=find_packages(),
     install_requires=[
         "watchdog"
@@ -15,5 +15,5 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.7',
+    python_requires='>=3.7'
 )
